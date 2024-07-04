a1638791de722a816e3e0a47425069ad36d0179f
lexilure <alexandrapadua01@gmail.com>
2024-07-04 18:30:58 +0800

    console

---
+++ CSOPESY_MCO1/ConsoleName.cpp
@@ -0,0 +1,15 @@
+#include "ConsoleName.h"
+
+ConsoleName::ConsoleName(std::string name)
+{
+	this->name = name;
+}
+
+ConsoleName::~ConsoleName()
+{
+}
+
+std::string ConsoleName::getName()
+{
+	return this->name;
+}
