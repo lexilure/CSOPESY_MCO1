1b38b978d2fa1c7e57ec7c560084b0f12881f8d5
lexilure <alexandrapadua01@gmail.com>
2024-07-06 20:55:10 +0800

    delete

--- CSOPESY_MCO1/MainStart.cpp
+++
@@ -1,8 +0,0 @@
-#include"MainConsole.h"  // Corrected include directive
-#include <iostream>
-MainConsole mainConsole;  // Create an instance of MainConsole
-int main(){
-    mainConsole.run();  // Run the main console application
-
-    return 0;
-}
